241e1f7b6c4c3d4e0de5f359caab123f6165b966
Pavelkv94 <pavel.kv94@yandex.by>
2022-09-29 19:35:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Test1 } from './components/test1';
 import { Test2 } from './components/test2';
 
 function App() {
-  console.log('Anna')
+  console.log('Anna Mint')
     return (
         <div className="App">
             <div className='wrapper'>
